c06d8f684a83fb993121428cb6302db69b0d1f60
Carol0161 <78099991+Carol0161@users.noreply.github.com>
2021-02-23 19:38:09 +0100

    HELLOWORD2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package edu.ucjc.dam1;
 
 public class HelloWorld {
 	  public static void main(String[] parametro) {
-	        System.out.println("Hola Mundo Java");
+	        System.out.println("Hola Mundo Java22");
 	    }
 }
